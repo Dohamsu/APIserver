8b46f3949489ae341177a068c9331cdf8b0b6736
dohamsu <rlawlsdnjswk@gmail.com>
2021-08-25 17:49:17 +0900

    방탈 기능 분기

--- source/services/croll_service.js
+++ source/services/croll_service.js
@@ -8,12 +8,41 @@ exports.getHtml = (param) => {
       
         console.log("param" + JSON.stringify(param));
 
-        let month = param.month;
-        let day   = param.day;
+        let month     =  param.month;
+        let day       =  param.day;
+        let site      =  param.site;
+        let keyword   =  param.keyword;
+        let searchUrl =  "";
+        console.log("site : " + param.site);
+
+        switch(site){
+            case "secretGarden_RiverTown" : 
+                searchUrl =  "http://www.secretgardenescape.com/reservation.html?k_shopno=9&rdate=2021-" +month +"-" + day+"&prdno=";
+                break;
+            case "secretGarden_Hyewha" : 
+                searchUrl =  "http://www.secretgardenescape.com/reservation.html?k_shopno=4&rdate=2021-" +month +"-" + day+"&prdno=";
+                break;
+            case "secretGarden_CenematicHyewha" : 
+                searchUrl =  "http://www.secretgardenescape.com/reservation.html?k_shopno=11&rdate=2021-" +month +"-" + day+"&prdno=";
+                break;
+            case "secretGarden" : 
+                searchUrl =  "http://www.secretgardenescape.com/reservation.html?k_shopno=9&rdate=2021-" +month +"-" + day+"&prdno=";
+                break;
+            case "pointNine_Gangnam1" : 
+                searchUrl =  "https://point-nine.com/layout/res/home.php?rev_days=2021-" +month +"-" + day+"&s_zizum=1&go=rev.make";
+                break;
+            case "pointNine_Gangnam2" : 
+                searchUrl =  "https://point-nine.com/layout/res/home.php?rev_days=2021-" +month +"-" + day+"&s_zizum=4&go=rev.make";
+                break;
+
+                //******************************************************************************************************************** */
+            case "matjip" : 
+                searchUrl =  "https://www.diningcode.com/isearch.php?query=" + keyword;
+                break;
+        }
 
 
-        let searchUrl =  "http://www.secretgardenescape.com/reservation.html?k_shopno=9&rdate=2021-" +month +"-" + day+"&prdno=";
-        console.log("searchUrl" + searchUrl);
+        console.log("searchUrl : " + searchUrl);
 
         const getContent = async () => {
             try{
@@ -36,24 +65,53 @@ exports.getHtml = (param) => {
                 .then(html =>{
                     let ulList = [];
                     const $ = cheerio.load(html);
-                    const $bodyList = $(".theme_box");
-                    // console.log($bodyList);
+                    var $bodyList = "";
 
-                    // console.log("===========  첫번째 돔 ======================");
-                    $bodyList.each(function(i, elem) {
-                        ulList[i] = {
-                            title: $(this).find('h3.h3_theme').first().text(),
-                            summary: $(this).find('p.tag_txt').text(),
-                            reservableTime:  
-                                $(this).find('ul.reserve_Time li a').not(".end").find(".time").text()
-                            
-                        };
-                    });
+                    console.log(site);
+                    switch(site){
+
+                        case "secretGarden_RiverTown" : 
+                        case "secretGarden_Hyewha" : 
+                        case "secretGarden_CenematicHyewha" : 
+
+                            $bodyList = $(".theme_box");
+
+                            $bodyList.each(function(i, elem) {
+                                ulList[i] = {
+                                    title: $(this).find('h3.h3_theme').first().text(),
+                                    summary: $(this).find('p.tag_txt').text(),
+                                    reservableTime:  
+                                        $(this).find('ul.reserve_Time li a').not(".end").find(".time").text(),
+                                    siteUrl : searchUrl
+                                    
+                                };
+                            });
+                        break;
+
+                        case "pointNine_Gangnam1" :
+                        case "pointNine_Gangnam2" : 
+                        
+                            $bodyList = $(".theme_box");
+
+                            $bodyList.each(function(i, elem) {
+                                ulList[i] = {
+                                    title: $(this).find('h3.h3_theme').first().text(),
+                                    // summary: $(this).find('p.tag_txt').text(),
+                                    reservableTime:  
+                                        $(this).find('ul.reserve_Time li a').not(".end").find(".time").text(),
+                                    siteUrl : searchUrl
+                                    
+                                };
+                            });
+
+                        break;
+
+                    }
                     const data = ulList.filter(n => n.title);
-                console.log("==============마지막 필터링 데이터 =================");
-                console.log(JSON.stringify(data));
-                
-                return data;
+                    console.log("==============마지막 필터링 데이터 =================");
+                    console.log(JSON.stringify(data));
+                    
+                    return data;
                 })
         }
         
